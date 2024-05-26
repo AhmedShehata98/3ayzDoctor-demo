b4ec6d7e3805f11c59959e1cdb09d84acfa9e0a1
Ahmed Shehata <ahmedshehataq98@gmail.com>
2024-05-26 17:44:47 +0300

    added withFetch() to app root

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,18 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppTranslatorModule } from '../shared/module/app-translator.module';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
     importProvidersFrom(HttpClientModule),
     importProvidersFrom(AppTranslatorModule.forRoot()),
   ],
